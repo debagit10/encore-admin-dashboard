c898ec1cb56f9f5587d3660868d3bc354dabf257
Josiah Elias <josiahelias11@gmail.com>
2025-06-26 09:29:03 +0100

    feat: add button group for date selection in Dashboard with state management

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,10 +1,21 @@
-import { Typography } from "@mui/material";
+import { Button, ButtonGroup, Typography } from "@mui/material";
 import Navbar from "../components/Navbar";
 import Pages from "../container/Pages";
 import { IoNotifications } from "react-icons/io5";
 import logo from "../assets/logo.png";
+import { useState } from "react";
 
 const Dashboard = () => {
+  const [period, setPeriod] = useState<number>(0);
+
+  const buttonGroup = [
+    { label: "All Date" },
+    { label: "12 Months" },
+    { label: "30 Days" },
+    { label: "7 Days" },
+    { label: "24 Hours" },
+  ];
+
   return (
     <Pages>
       <Navbar page="Dashboard">
@@ -25,6 +36,55 @@ const Dashboard = () => {
           </div>
         </div>
       </Navbar>
+
+      <div className="px-[33.5px] pt-[.5rem]">
+        <div className="flex justify-between items-center">
+          <div className="flex flex-col gap-[8px]">
+            <Typography fontWeight={500} fontSize={24} color="#1D1F2C">
+              Welcome Back Lookman 👋🏼
+            </Typography>
+            <Typography fontWeight={400} fontSize={14} color="#667085">
+              We have your reports ready , Get ready to change the world with AI
+            </Typography>
+          </div>
+
+          <div>
+            <ButtonGroup
+              variant="outlined"
+              sx={{
+                borderRadius: "8px",
+                border: "1px solid #E0E2E7",
+                "& .MuiButtonGroup-grouped": {
+                  margin: "3px",
+                  border: "none",
+                  borderRadius: "8px !important",
+                  color: "#667085",
+                },
+                "& .MuiButton-outlined": {},
+              }}
+            >
+              {buttonGroup.map((button, index) => (
+                <Button
+                  key={index}
+                  sx={{
+                    textTransform: "capitalize",
+                    backgroundColor: period === index ? "#E7F3FD" : "inherit",
+                  }}
+                  onClick={() => setPeriod(index)}
+                >
+                  <Typography
+                    color={period === index ? "#0167C4" : "#667085"}
+                    fontWeight={period === index ? 600 : 400}
+                    fontSize={14}
+                  >
+                    {button.label}
+                  </Typography>
+                </Button>
+              ))}
+            </ButtonGroup>
+          </div>
+        </div>
+      </div>
     </Pages>
   );
 };
